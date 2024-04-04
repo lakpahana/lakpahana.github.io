b330d6172442d5ddafdcfdec894d08622630132f
lakpahana <agslakpahana@gmail.com>
2024-04-04 15:33:34 +0530

    shortner2

--- goto/script.js
+++ goto/script.js
@@ -1,1 +1,6 @@
-console.log("scriot")
+console.log("Script loaded");
+var url = window.location.href;
+var urlSplit = url.split("/goto");
+var url = urlSplit[1];
+// console.log(url.split("/")[1]);
+console.log("You are redirected to: " + url.split("/")[1]);
